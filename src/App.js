749b4b1cc2ff775780149110186c853b2d57f6f3
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2024-07-10 12:38:54 +0530

    added banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import Banner from "./Components/Banner";
 import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      This is the app component for portfolio website
       <NavBar />
+      <Banner />
     </div>
   );
 }
